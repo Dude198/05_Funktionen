8faa720964b176e7e59f5667c011ee1a71d9b5ee
Marcel <marcel.eggert1@outlook,de>
2022-11-11 10:48:32 +0100

    ????

--- script_01.js
+++ script_01.js
@@ -2,3 +2,35 @@
 
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+// // Funktionsrumpf (body) | callee
+// function test()
+// {
+//     console.log("Hallo Marcel!");    
+// }
+
+// // Funktionsaufruf (call)
+// test();
+
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// ausgabeNamen();
+// // console.log(firstName); // Fehler: Scope!
+// function ausgabeNamen() {
+//     let firstName = "Marcel";
+//     console.log("Hallo " + firstName + "!");    
+// }
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von Aussen
+
+ausgabeNamenParam("Sabine");// Argument
+// Argumente sind Daten für Parameter
+
+function ausgabeNamenParam(firstName) // Parameter
+{
+    console.log("Hallo " + firstName + "!");    
+}
